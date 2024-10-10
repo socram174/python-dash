9043211c05eb597e4a845f95c64a75ad69aad18e
socram174 <socram174@hotmail.com>
2024-10-10 15:07:16 -0300

    port update 2

--- main.py
+++ main.py
@@ -2,21 +2,25 @@
 from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import pandas as pd
 import plotly.express as px
+from waitress import serve
 
 # Incorporate data
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
 
 # Initialize the app
-app = Dash()
+app = Dash(__name__)
+
+# Expose the Flask server
+server = app.server
 
 # App layout
-app.layout = [
+app.layout = html.Div([
     html.Div(children='My First App with Data, Graph, and Controls'),
     html.Hr(),
     dcc.RadioItems(options=['pop', 'lifeExp', 'gdpPercap'], value='lifeExp', id='my-final-radio-item-example'),
     dash_table.DataTable(data=df.to_dict('records'), page_size=6),
     dcc.Graph(figure={}, id='my-final-graph-example')
-]
+])
 
 # Add controls to build the interaction
 @callback(
@@ -29,4 +33,5 @@ def update_graph(col_chosen):
 
 # Run the app
 if __name__ == '__main__':
-    app.run(debug=True, port=10000)
+    # Use Waitress for production
+    serve(server, host='0.0.0.0', port=10000)
